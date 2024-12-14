ad33c4ba8b1311ebce438c17458c01f00641398a
mquangcao <mquangcao38@gmail.com>
2024-12-14 23:13:02 +0700

    feat : add edit profile and account info UI

--- app/src/main/java/com/android_ai/csc13009/app/presentation/activity/DashboardActivity.kt
+++ app/src/main/java/com/android_ai/csc13009/app/presentation/activity/DashboardActivity.kt
@@ -1,21 +1,46 @@
 package com.android_ai.csc13009.app.presentation.activity
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.android_ai.csc13009.R
+import com.android_ai.csc13009.app.presentation.fragment.AccountFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var bottomNavView : BottomNavigationView
+    private lateinit var frame : FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        bottomNavView = findViewById(R.id.bottom_nav)
+        frame = findViewById(R.id.nav_host_fragment)
+
+        bottomNavView.setOnItemSelectedListener  {
+            when (it.itemId) {
+                R.id.home -> {
+                    // Handle Home
+                    true
+                }
+                R.id.account -> {
+                    loadFragment(AccountFragment())
+                    true
+                }
+
+                else -> false
+            }
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment).commit()
     }
 }
---
+++ app/src/main/java/com/android_ai/csc13009/app/presentation/activity/EditProfileActivity.kt
@@ -0,0 +1,64 @@
+package com.android_ai.csc13009.app.presentation.activity
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.android_ai.csc13009.R
+import com.google.android.material.button.MaterialButton
+
+class EditProfileActivity : AppCompatActivity() {
+    private lateinit var btnArrowBack : ImageButton
+    private lateinit var dialog : Dialog
+    private lateinit var btnConfirm : Button
+    private lateinit var btnCancel : MaterialButton
+    private lateinit var btnLogout : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_profile)
+
+        btnArrowBack = findViewById(R.id.btnArrowBack)
+        btnArrowBack.setOnClickListener {
+            finish()
+        }
+
+        dialog = Dialog(this)
+        dialog.setContentView(R.layout.custom_dialog_confirm_logout)
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawableResource(R.drawable.custom_dialog_bg)
+        dialog.setCancelable(false)
+
+        btnConfirm = dialog.findViewById(R.id.btnConfirm)
+        btnCancel = dialog.findViewById(R.id.btnCancel)
+
+        btnConfirm.setOnClickListener {
+            Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        btnCancel.setOnClickListener {
+            Toast.makeText(this, "cancel", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        btnLogout = findViewById(R.id.btnLogout)
+
+        btnLogout.setOnClickListener {
+            dialog.show()
+        }
+
+    }
+}
