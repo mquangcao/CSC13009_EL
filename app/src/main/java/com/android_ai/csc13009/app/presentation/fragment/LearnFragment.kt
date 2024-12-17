9c09ebb092a78c8529b742ecbd46ed6050a311e2
mquangcao <mquangcao38@gmail.com>
2024-12-17 21:13:55 +0700

    feat : add UI learnFragment

--- app/src/main/java/com/android_ai/csc13009/app/presentation/fragment/LearnFragment.kt
+++ app/src/main/java/com/android_ai/csc13009/app/presentation/fragment/LearnFragment.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.android_ai.csc13009.R
 import com.android_ai.csc13009.app.presentation.activity.VocabularyActivity
 
 
 class LearnFragment : Fragment() {
+    private lateinit var learnVocab : CardView
+    private lateinit var learnGrammar : CardView
+    private lateinit var book : CardView
 
 
     override fun onCreateView(
@@ -19,8 +23,26 @@ class LearnFragment : Fragment() {
     ): View? {
 
         val view =  inflater.inflate(R.layout.fragment_learn, container, false)
-        val intent = Intent(view.context, VocabularyActivity::class.java)
-        startActivity(intent)
+
+        learnVocab = view.findViewById(R.id.learnVocab)
+        learnGrammar = view.findViewById(R.id.learnGrammar)
+        book = view.findViewById(R.id.book)
+
+
+        learnVocab.setOnClickListener {
+            val intent = Intent(view.context, VocabularyActivity::class.java)
+            startActivity(intent)
+        }
+
+        learnGrammar.setOnClickListener {
+            val intent = Intent(view.context, VocabularyActivity::class.java)
+            startActivity(intent)
+        }
+
+        book.setOnClickListener {
+            val intent = Intent(view.context, VocabularyActivity::class.java)
+            startActivity(intent)
+        }
 
         return view
     }
