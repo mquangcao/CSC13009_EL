e30a398aadad9b0e1266b813b1bbb292d9b5890e
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-01-12 14:28:05 +0700

    fix: input position

---
+++ app/src/main/java/com/android_ai/csc13009/app/presentation/activity/LearnListeningActivity.kt
@@ -0,0 +1,29 @@
+package com.android_ai.csc13009.app.presentation.activity
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android_ai.csc13009.R
+
+class LearnListeningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lear_vocab)
+
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        val btnBack = findViewById<ImageView>(R.id.arrow_back)
+
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+    }
+}
