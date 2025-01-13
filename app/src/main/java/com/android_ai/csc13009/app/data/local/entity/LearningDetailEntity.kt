6ce9e575f65b15ad2fd86416c60e948998ebd42c
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-01-13 14:03:53 +0700

    feat: saving answer  Note: temporary halt development on this branch for further consultation with other members

--- app/src/main/java/com/android_ai/csc13009/app/data/local/entity/LearningDetailEntity.kt
+++ app/src/main/java/com/android_ai/csc13009/app/data/local/entity/LearningDetailEntity.kt
@@ -9,6 +9,6 @@ data class LearningDetailEntity(
     val id: Int = 0,         // ID tự tăng
     val date: String,        // Ngày học (định dạng ISO 8601, ví dụ: "2024-12-04")
     val questionId: Int,     // ID của câu hỏi
-    val isCorrect: Boolean,  // Kết quả đúng/sai
+    var isCorrect: Boolean,  // Kết quả đúng/sai
     val userId: String       // ID của người dùng
 ) : Serializable
--- app/src/main/java/com/android_ai/csc13009/app/presentation/activity/ILearnActivity.kt
+++ app/src/main/java/com/android_ai/csc13009/app/presentation/activity/ILearnActivity.kt
@@ -1,22 +1,20 @@
 package com.android_ai.csc13009.app.presentation.activity;
 
-import android.os.Bundle
-import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android_ai.csc13009.R
-import com.android_ai.csc13009.app.data.local.dao.QuestionDao
+import com.android_ai.csc13009.app.data.local.dao.LearningDetailDao
+import com.android_ai.csc13009.app.data.local.dao.LessonDao
 import com.android_ai.csc13009.app.domain.models.Answer
 import com.android_ai.csc13009.app.domain.models.Lesson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 interface ILearnActivity {
 
     var lesson: Lesson
     var currentQuestionIndex: Int
-//    var questionRespository: QuestionDao
-//    var lessonRepository: Repository
+    var lessonDao: LessonDao
+    var learningDetailDao: LearningDetailDao
+    var userId: String
 
     public fun fetchLesson(lesson: Lesson) {
         this.lesson = lesson
@@ -29,6 +27,12 @@ interface ILearnActivity {
             lesson.questionSuccess++
         }
         lesson.questions[currentQuestionIndex].isCorrect = answer.isCorrect
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val learningDetail = learningDetailDao.getLearningDetailByQuestionAndUser(lesson.questions[currentQuestionIndex].id, userId)
+            learningDetail.isCorrect = answer.isCorrect
+            learningDetailDao.insertLearningDetail(learningDetail)
+        }
         nextQuestion()
     }
 
@@ -36,7 +40,7 @@ interface ILearnActivity {
         currentQuestionIndex++
         if (currentQuestionIndex >= lesson.questions.size) {
 //            nextLesson()
-            saveProgress()
+//            saveProgress()
         }
     }
 
@@ -54,9 +58,9 @@ interface ILearnActivity {
 
     }
 
-    fun saveProgress() {
-        
-    }
+//    fun saveProgress() {
+//
+//    }
 
 //    fun
 }
