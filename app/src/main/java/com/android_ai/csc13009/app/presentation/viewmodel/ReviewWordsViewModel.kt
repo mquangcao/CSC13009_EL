defd842d2898e0bcf3520d28990a4bebe62fb615
Justiph <124757476+Justiph@users.noreply.github.com>
2025-01-19 12:12:50 +0700

    feat: mistakes

---
+++ app/src/main/java/com/android_ai/csc13009/app/presentation/viewmodel/ReviewWordsViewModel.kt
@@ -0,0 +1,4 @@
+package com.android_ai.csc13009.app.presentation.viewmodel
+
+class ReviewWordsViewModel {
+}
---
+++ app/src/main/java/com/android_ai/csc13009/app/utils/adapter/MistakeAdapter.kt
@@ -0,0 +1,4 @@
+package com.android_ai.csc13009.app.utils.adapter
+
+class MistakeAdapter {
+}
